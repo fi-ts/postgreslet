8ed472333a7cdd8e4eaed88c35d337bdb9650be3
Philipp Eberle <eberlep@gmail.com>
2020-11-30 21:26:00 +0100

    rough first draft

--- controllers/status_controller.go
+++ controllers/status_controller.go
@@ -20,11 +20,14 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	zalando "github.com/zalando/postgres-operator/pkg/apis/acid.zalan.do/v1"
+
+	pg "github.com/fi-ts/postgres-controller/api/v1"
 )
 
 // StatusReconciler reconciles a Postgresql object
@@ -34,19 +37,60 @@ type StatusReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+// Reconcile updates the status of the remote Postgres object based on the status of the local zalando object.
 // +kubebuilder:rbac:groups=acid.zalan.do,resources=postgresqls,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=acid.zalan.do,resources=postgresqls/status,verbs=get;update;patch
-
 func (r *StatusReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
+	ctx := context.Background()
 	log := r.Log.WithValues("postgresql", req.NamespacedName)
 
-	// your logic here
-	log.Info("Reconcile!!!")
+	log.Info("fetching postgresql")
+	instance := &zalando.Postgresql{}
+	if err := r.Client.Get(ctx, req.NamespacedName, instance); err != nil {
+		if !errors.IsNotFound(err) {
+			return ctrl.Result{}, err
+		}
+		// TODO discuss. the zalando object should only be deleted as a result of the remote object being deleted, so in
+		// theory, no action should be necessary here? however, if the zalando object was removed manually for some
+		// reason, it should probably be recreated here?
+		log.Info("status changed to Deleted?")
+		return ctrl.Result{}, nil
+	}
+	log.Info("Got status update", "PostgresClusterStatus", instance.Status.PostgresClusterStatus)
+
+	log.Info("fetching all owners")
+	owners := &pg.PostgresList{}
+	if err := r.Client.List(ctx, owners); err != nil {
+		log.Info("error fetching all owners")
+		return ctrl.Result{}, err
+	}
+
+	// FIXME currently hardcoded!!!
+	derivedOwnerName := "sample-name"
+	var owner pg.Postgres
+	for _, owner = range owners.Items {
+		// TODO switch to UID
+		if owner.Name != derivedOwnerName {
+			log.Info("owner not a match", "remote", owner)
+			continue
+		}
+		// TODO also check ProjectID
+
+		log.Info("Found owner", "owner", owner)
+		break
+	}
+
+	owner.Status.Description = instance.Status.PostgresClusterStatus
+	log.Info("Updating remote", "remote", owner)
+	if err := r.Client.Update(ctx, &owner); err != nil {
+		log.Error(err, "failed to update remote object")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
+// SetupWithManager registers this controller for reconciliation of Postgresql resources
 func (r *StatusReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&zalando.Postgresql{}).
