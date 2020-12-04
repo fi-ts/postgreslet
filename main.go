c4232406babfc3ac97ac767fc8a196bd0a9f27ca
Lim, Kian-An <lim.kian.an@gmx.de>
2020-12-05 00:20:47 +0100

    Fix ns bugs, make sure resouces exist before usage

--- main.go
+++ main.go
@@ -20,20 +20,24 @@ import (
 	"context"
 	"flag"
 	"io/ioutil"
+	"log"
 	"os"
+	"time"
 
 	"github.com/metal-stack/v"
+	zalando "github.com/zalando/postgres-operator/pkg/apis/acid.zalan.do/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/wait"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	zalando "github.com/zalando/postgres-operator/pkg/apis/acid.zalan.do/v1"
 
 	databasev1 "github.com/fi-ts/postgres-controller/api/v1"
 	"github.com/fi-ts/postgres-controller/controllers"
@@ -64,7 +68,8 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	conf := ctrl.GetConfigOrDie()
+	mgr, err := ctrl.NewManager(conf, ctrl.Options{
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
 		Port:               9443,
@@ -76,12 +81,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	objs, err := installExternalYAML("./external.yaml", "example-partition", mgr.GetClient())
+	// Use no-cache client to avoid waiting.
+	newClient, err := client.New(conf, client.Options{})
+	if err != nil {
+		setupLog.Error(err, "unable to create a new client")
+		os.Exit(1)
+	}
+	objs, err := installExternalYAML("./external.yaml", "example-partition", newClient)
 	if err != nil {
 		setupLog.Error(err, "unable to install external YAML")
 		os.Exit(1)
 	}
-	defer uninstallExternalYaml(objs, mgr.GetClient())
+	defer uninstallExternalYaml(objs, newClient)
 
 	if err = (&controllers.PostgresReconciler{
 		Client: mgr.GetClient(),
@@ -106,6 +117,7 @@ func main() {
 	}
 }
 
+// todo: Refactor
 func installExternalYAML(fileName, partitionID string, k8sClient client.Client) (objs []runtime.Object, err error) {
 	bb, err := ioutil.ReadFile(fileName)
 	if err != nil {
@@ -141,10 +153,58 @@ func installExternalYAML(fileName, partitionID string, k8sClient client.Client) 
 			return
 		}
 
+		// Remove annotations.
+		if err = accessor.SetAnnotations(obj, nil); err != nil {
+			return
+		}
+
+		// Add the namespace to the `ServiceAccount` in the `ClusterRoleBinding`
+		if v, ok := obj.(*v1.ClusterRoleBinding); ok {
+			for i, s := range v.Subjects {
+				if s.Kind == "ServiceAccount" {
+					v.Subjects[i].Namespace = partitionID
+				}
+			}
+			log.Println("clusterrolebinding", v)
+		}
+
 		if err = k8sClient.Create(ctx, obj); err != nil {
 			return
 		}
 		objs = append(objs, obj)
+	}
+
+	// Wait till there's at least one `postgres-operator` pod.
+	pods := &corev1.PodList{}
+	if err = wait.Poll(time.Second, 30*time.Second, func() (bool, error) {
+		if err := k8sClient.List(ctx, pods, client.MatchingLabels{"name": "postgres-operator"}); err != nil {
+			if errors.IsNotFound(err) {
+				return false, nil
+			}
+			return false, err
+		}
+		if len(pods.Items) == 0 {
+			return false, nil
+		}
+		return true, nil
+	}); err != nil {
+		return
+	}
+
+	// Wait till at least one such pod is running.
+	if err = wait.Poll(time.Second, 60*time.Second, func() (bool, error) {
+		for _, pod := range pods.Items {
+			newPod := &corev1.Pod{}
+			if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: pod.Namespace, Name: pod.Name}, newPod); err != nil {
+				return false, err
+			}
+			if newPod.Status.Phase == corev1.PodRunning {
+				return true, nil
+			}
+		}
+		return false, nil
+	}); err != nil {
+		return
 	}
 
 	return
