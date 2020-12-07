62d9df7fa7750fdfbe433957cf812d136624eb34
Lim, Kian-An <lim.kian.an@gmx.de>
2020-12-07 22:31:25 +0100

    Move external-yaml-related files to its own pkg

--- main.go
+++ main.go
@@ -17,22 +17,12 @@ limitations under the License.
 package main
 
 import (
-	"context"
 	"flag"
-	"io/ioutil"
-	"log"
 	"os"
-	"time"
 
 	"github.com/metal-stack/v"
 	zalando "github.com/zalando/postgres-operator/pkg/apis/acid.zalan.do/v1"
-	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/rbac/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"k8s.io/apimachinery/pkg/util/wait"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -41,6 +31,7 @@ import (
 
 	databasev1 "github.com/fi-ts/postgres-controller/api/v1"
 	"github.com/fi-ts/postgres-controller/controllers"
+	yaml "github.com/fi-ts/postgres-controller/pkg/externalyaml"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -87,12 +78,12 @@ func main() {
 		setupLog.Error(err, "unable to create a new client")
 		os.Exit(1)
 	}
-	objs, err := installExternalYAML("./external.yaml", "example-partition", newClient)
+	objs, err := yaml.InstallExternalYAML("./external.yaml", "example-partition", newClient, scheme)
 	if err != nil {
 		setupLog.Error(err, "unable to install external YAML")
 		os.Exit(1)
 	}
-	defer uninstallExternalYaml(objs, newClient)
+	defer yaml.UninstallExternalYaml(objs, newClient)
 
 	if err = (&controllers.PostgresReconciler{
 		Client: mgr.GetClient(),
@@ -115,106 +106,4 @@ func main() {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
-}
-
-// todo: Refactor
-func installExternalYAML(fileName, partitionID string, k8sClient client.Client) (objs []runtime.Object, err error) {
-	bb, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return
-	}
-
-	ctx := context.Background()
-
-	// Make sure the namespace `partitionID` exists.
-	ns := &corev1.Namespace{}
-	if err = k8sClient.Get(ctx, client.ObjectKey{Name: partitionID}, &corev1.Namespace{}); err != nil {
-		ns.Name = partitionID
-		if err = k8sClient.Create(ctx, ns); err != nil {
-			return
-		}
-	}
-
-	// Convert to a list of YAMLs.
-	deserializer := serializer.NewCodecFactory(scheme).UniversalDeserializer()
-	list := &corev1.List{}
-	if _, _, err = deserializer.Decode(bb, nil, list); err != nil {
-		return
-	}
-
-	// Decode each YAML to `runtime.Object`, add the namespace to it and install it.
-	accessor := meta.NewAccessor()
-	for _, item := range list.Items {
-		obj, _, er := deserializer.Decode(item.Raw, nil, nil)
-		if er != nil {
-			return objs, er
-		}
-		if err = accessor.SetNamespace(obj, partitionID); err != nil {
-			return
-		}
-
-		// Remove annotations.
-		if err = accessor.SetAnnotations(obj, nil); err != nil {
-			return
-		}
-
-		// Add the namespace to the `ServiceAccount` in the `ClusterRoleBinding`
-		if v, ok := obj.(*v1.ClusterRoleBinding); ok {
-			for i, s := range v.Subjects {
-				if s.Kind == "ServiceAccount" {
-					v.Subjects[i].Namespace = partitionID
-				}
-			}
-			log.Println("clusterrolebinding", v)
-		}
-
-		if err = k8sClient.Create(ctx, obj); err != nil {
-			return
-		}
-		objs = append(objs, obj)
-	}
-
-	// Wait till there's at least one `postgres-operator` pod.
-	pods := &corev1.PodList{}
-	if err = wait.Poll(time.Second, 30*time.Second, func() (bool, error) {
-		if err := k8sClient.List(ctx, pods, client.MatchingLabels{"name": "postgres-operator"}); err != nil {
-			if errors.IsNotFound(err) {
-				return false, nil
-			}
-			return false, err
-		}
-		if len(pods.Items) == 0 {
-			return false, nil
-		}
-		return true, nil
-	}); err != nil {
-		return
-	}
-
-	// Wait till at least one such pod is running.
-	if err = wait.Poll(time.Second, 60*time.Second, func() (bool, error) {
-		for _, pod := range pods.Items {
-			newPod := &corev1.Pod{}
-			if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: pod.Namespace, Name: pod.Name}, newPod); err != nil {
-				return false, err
-			}
-			if newPod.Status.Phase == corev1.PodRunning {
-				return true, nil
-			}
-		}
-		return false, nil
-	}); err != nil {
-		return
-	}
-
-	return
-}
-
-func uninstallExternalYaml(objs []runtime.Object, k8sClient client.Client) error {
-	for _, obj := range objs {
-		if err := k8sClient.Delete(context.Background(), obj); err != nil {
-			return err
-		}
-	}
-	return nil
 }
---
+++ pkg/externalyaml/externalyaml.go
@@ -0,0 +1,133 @@
+package externalyaml
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func InstallExternalYAML(fileName, namespace string, k8sClient client.Client, scheme *runtime.Scheme) (objs []runtime.Object, err error) {
+	bb, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return
+	}
+
+	ctx := context.Background()
+
+	// Make sure the namespace `partitionID` exists.
+	ns := &corev1.Namespace{}
+	if err = k8sClient.Get(ctx, client.ObjectKey{Name: namespace}, &corev1.Namespace{}); err != nil {
+		ns.Name = namespace
+		if err = k8sClient.Create(ctx, ns); err != nil {
+			return
+		}
+	}
+
+	// Convert to a list of YAMLs.
+	deserializer := serializer.NewCodecFactory(scheme).UniversalDeserializer()
+	list := &corev1.List{}
+	if _, _, err = deserializer.Decode(bb, nil, list); err != nil {
+		return
+	}
+
+	// Decode each YAML to `runtime.Object`, add the namespace to it and install it.
+	accessor := meta.NewAccessor()
+	for _, item := range list.Items {
+		obj, _, er := deserializer.Decode(item.Raw, nil, nil)
+		if er != nil {
+			return objs, er
+		}
+
+		// Remove annotations.
+		if err = accessor.SetAnnotations(obj, nil); err != nil {
+			return
+		}
+
+		if err = setNamespace(obj, namespace, accessor); err != nil {
+			return
+		}
+
+		if err = k8sClient.Create(ctx, obj); err != nil {
+			return
+		}
+		objs = append(objs, obj)
+	}
+
+	if err = waitTillZalandoPostgresOperatorReady(ctx, time.Minute, time.Second, k8sClient); err != nil {
+		return
+	}
+
+	return
+}
+
+func UninstallExternalYaml(objs []runtime.Object, k8sClient client.Client) error {
+	for _, obj := range objs {
+		if err := k8sClient.Delete(context.Background(), obj); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func setNamespace(obj runtime.Object, namespace string, accessor meta.MetadataAccessor) error {
+	if err := accessor.SetNamespace(obj, namespace); err != nil {
+		return err
+	}
+
+	// Add the namespace to the `ServiceAccount` in the `ClusterRoleBinding`
+	if v, ok := obj.(*v1.ClusterRoleBinding); ok {
+		for i, s := range v.Subjects {
+			if s.Kind == "ServiceAccount" {
+				v.Subjects[i].Namespace = namespace
+			}
+		}
+		log.Println("clusterrolebinding", v)
+	}
+
+	return nil
+}
+
+func waitTillZalandoPostgresOperatorReady(ctx context.Context, timeout time.Duration, period time.Duration, k8sClient client.Client) (err error) {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	// Wait till there's at least one `postgres-operator` pod with status `running`.
+	if err = wait.Poll(period, timeout, func() (bool, error) {
+		// Fetch the pods with the matching labels.
+		pods := &corev1.PodList{}
+		if err := k8sClient.List(ctx, pods, client.MatchingLabels{"name": "postgres-operator"}); err != nil {
+			// `Not found` isn't an error.
+			return false, client.IgnoreNotFound(err)
+		}
+		if len(pods.Items) == 0 {
+			return false, nil
+		}
+
+		// Roll the list to examine the status.
+		for _, pod := range pods.Items {
+			newPod := &corev1.Pod{}
+			if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: pod.Namespace, Name: pod.Name}, newPod); err != nil {
+				return false, err
+			}
+			if newPod.Status.Phase == corev1.PodRunning {
+				return true, nil
+			}
+		}
+
+		// Nothing found. Poll after the period.
+		return false, nil
+	}); err != nil {
+		return
+	}
+
+	return
+}
