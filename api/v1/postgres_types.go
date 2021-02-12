c70f12389a1f5da90fc07275ad8cf0a0a5bd7083
Stefan Majer <stefan.majer@f-i-ts.de>
2021-02-12 13:35:00 +0100

    add references to the secrets where the backup and user credentials are stored (#92)

--- api/v1/postgres_types.go
+++ api/v1/postgres_types.go
@@ -74,6 +74,8 @@ type PostgresSpec struct {
 	Backup *Backup `json:"backup,omitempty"`
 	// AccessList defines access restrictions
 	AccessList *AccessList `json:"accessList,omitempty"`
+	// SecretName reference to the secret where the user credentials are stored
+	SecretName string `json:"secretname,omitempty"`
 }
 
 // AccessList defines the type of restrictions to access the database
@@ -90,6 +92,8 @@ type Backup struct {
 	Retention int32 `json:"retention,omitempty"`
 	// Schedule defines how often a backup should be made, in cron format
 	Schedule string `json:"schedule,omitempty"`
+	// SecretName reference to the secret where the backup credentials are stored
+	SecretName string `json:"secretname,omitempty"`
 }
 
 // Size defines the size aspects of the database
