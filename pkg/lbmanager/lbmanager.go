94bff3ae961e2ece023f7fcb8f5a5633a4a18440
Philipp Eberle <eberlep@gmail.com>
2021-02-25 10:46:44 +0100

    Check list size before accessing it

--- pkg/lbmanager/lbmanager.go
+++ pkg/lbmanager/lbmanager.go
@@ -88,8 +88,9 @@ func (m *LBManager) nextFreeSocket(ctx context.Context) (string, int32, error) {
 	isOccupied := make([]bool, int(m.PortRangeSize))
 	for i := range lbs.Items {
 		svc := lbs.Items[i]
-		// TODO panic: runtime error: index out of range [-23920]
-		isOccupied[svc.Spec.Ports[0].Port-m.PortRangeStart] = true
+		if len(svc.Spec.Ports) > 0 {
+			isOccupied[svc.Spec.Ports[0].Port-m.PortRangeStart] = true
+		}
 		if svc.Spec.LoadBalancerIP != "" {
 			existingLBIP = svc.Spec.LoadBalancerIP
 		}
