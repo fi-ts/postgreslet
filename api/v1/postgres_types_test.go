c1c3433e39fce928a7f85e795a83d307db79ede4
eberlep <eberlep@users.noreply.github.com>
2021-07-01 12:11:33 +0200

    Once again, modify the generaetd IDs. This time, make sure it does no… (#243)

--- api/v1/postgres_types_test.go
+++ api/v1/postgres_types_test.go
@@ -7,7 +7,10 @@
 package v1
 
 import (
+	"regexp"
 	"testing"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Test_setSharedBufferSize(t *testing.T) {
@@ -79,6 +82,133 @@ func Test_setSharedBufferSize(t *testing.T) {
 				if tt.expected != "" {
 					t.Errorf("Conversion failed")
 				}
+			}
+		})
+	}
+}
+
+func TestPostgres_generateTeamID(t *testing.T) {
+	tests := []struct {
+		name      string
+		projectID string
+	}{
+		{
+			name:      "beginning with letter",
+			projectID: "abc",
+		},
+		{
+			name:      "beginning with number",
+			projectID: "1bc",
+		},
+		{
+			name:      "contains number in the middle",
+			projectID: "a1c",
+		},
+		{
+			name:      "ends with number",
+			projectID: "ab1",
+		},
+		{
+			name:      "contains non-alphanumeric characters",
+			projectID: "ab-cd.ef@12",
+		},
+		{
+			name:      "contains non-alphanumeric characters",
+			projectID: "12@ab-cd.ef",
+		},
+		{
+			name:      "more than 16 chars long",
+			projectID: "abcdefabcdefabcdefabcdefabcdefab",
+		},
+		{
+			name:      "more than 16 numbers long",
+			projectID: "12345678901234567890123456789012",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt // pin!
+		t.Run(tt.name, func(t *testing.T) {
+			var dnsRegExp *regexp.Regexp = regexp.MustCompile("^[a-z]([-a-z0-9]*[a-z0-9])?$")
+			p := &Postgres{
+				Spec: PostgresSpec{
+					ProjectID: tt.projectID,
+				},
+			}
+			got := p.generateTeamID()
+			if !dnsRegExp.MatchString(got) {
+				t.Errorf("Postgres.generateTeamID() got %v, not a valid DNS name", got)
+			}
+		})
+	}
+}
+func TestPostgres_ToPeripheralResourceName(t *testing.T) {
+	tests := []struct {
+		name         string
+		projectID    string
+		postgresName string
+	}{
+		{
+			name:         "letters",
+			projectID:    "abc",
+			postgresName: "bca",
+		},
+		{
+			name:         "numbers at the beginning",
+			projectID:    "1bc",
+			postgresName: "1cb",
+		},
+		{
+			name:         "numbers in the middle",
+			projectID:    "a1c",
+			postgresName: "c1a",
+		},
+		{
+			name:         "numbers at the end",
+			projectID:    "ab1",
+			postgresName: "ba1",
+		},
+		{
+			name:         "non-alphanumeric characters",
+			projectID:    "ab-cd.ef@12",
+			postgresName: "ba.dc-fe@21",
+		},
+		{
+			name:         "non-alphanumeric characters beginning with numbers",
+			projectID:    "12@ab-cd.ef",
+			postgresName: "21@ba-dc.fe",
+		},
+		{
+			name:         "too many letters",
+			projectID:    "abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef",
+			postgresName: "abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef",
+		},
+		{
+			name:         "too many numbers",
+			projectID:    "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890",
+			postgresName: "123456789012345678901234567890121234567890123456789012345678901212345678901234567890123456789012",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt // pin!
+		t.Run(tt.name, func(t *testing.T) {
+			var dnsRegExp *regexp.Regexp = regexp.MustCompile("^[a-z]([-a-z0-9]*[a-z0-9])?$")
+			p := &Postgres{
+				ObjectMeta: v1.ObjectMeta{
+					Name: tt.postgresName,
+				},
+				Spec: PostgresSpec{
+					ProjectID: tt.projectID,
+				},
+			}
+			got := p.ToPeripheralResourceName()
+			if !dnsRegExp.MatchString(got) {
+				t.Errorf("Postgres.ToPeripheralResourceName() got %v, not a valid DNS name", got)
+			}
+			//This resource name will be used as part of the name of other resources, hence we need to limit it's length,
+			// e.g. "postgres.bce25ade7552494c-33d21de46d284ea6bec0.credentials"
+			maxLen := 37
+			if len(got) > maxLen {
+				t.Errorf("Postgres.ToPeripheralResourceName() got %v, more than %d characters long", got, maxLen)
 			}
 		})
 	}
