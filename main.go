bfc6e6b2d314eee6939684afa1f79a3b754db539
Philipp Eberle <eberlep@gmail.com>
2022-10-14 11:21:05 +0200

    Change default value

--- main.go
+++ main.go
@@ -225,7 +225,7 @@ func main() {
 
 	etcdImage = viper.GetString(etcdImageFlg)
 	etcdBackupSidecarImage = viper.GetString(etcdBackupSidecarImageFlg)
-	viper.SetDefault(etcdBackupSecretNameFlg, "etcd-backup-restore-s3-config") // TODO remove
+	viper.SetDefault(etcdBackupSecretNameFlg, "pgaas-etcd-s3-credentials")
 	etcdBackupSecretName = viper.GetString(etcdBackupSecretNameFlg)
 
 	viper.SetDefault(etcdPSPNameFlg, pspName)
