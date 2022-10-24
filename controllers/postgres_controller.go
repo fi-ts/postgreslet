48cd617c6c933b91a7f217163dd68026c2e2ef98
eberlep <eberlep@users.noreply.github.com>
2022-10-24 18:35:44 +0200

    Create postgres-pod-config when missing (#444)
    * Create postgres-pod-config when missing
    
    * Create postgres-pod-config when missing
    
    * Create postgres-pod-config when missing
    
    * Revert "Create postgres-pod-config when missing"
    
    This reverts commit 80abfc72b019d515b8f0bd55868db1f7f8b02ad7.
    
    * Create postgres-pod-config when missing
    
    * Revert "Create postgres-pod-config when missing"
    
    This reverts commit f06c73b6d750dacf526c5d2a6afc8a1cb894a930.
    
    * Revert "Revert "Create postgres-pod-config when missing""
    
    This reverts commit 1723f97ea9d44271d3c2ce277879bcb2d94d4b6b.
    
    * Slightly improve logging
    
    * Slightly improve logging

--- controllers/postgres_controller.go
+++ controllers/postgres_controller.go
@@ -391,7 +391,7 @@ func (r *PostgresReconciler) ensureZalandoDependencies(ctx context.Context, p *p
 }
 
 func (r *PostgresReconciler) updatePodEnvironmentConfigMap(ctx context.Context, p *pg.Postgres) error {
-	log := r.Log.WithValues("postgres", p.UID)
+	log := r.Log.WithValues("postgres", p.Name)
 	if p.Spec.BackupSecretRef == "" {
 		log.Info("No configured backupSecretRef found, skipping configuration of postgres backup")
 		return nil
@@ -457,7 +457,15 @@ func (r *PostgresReconciler) updatePodEnvironmentConfigMap(ctx context.Context, 
 		Namespace: p.ToPeripheralResourceNamespace(),
 	}
 	if err := r.SvcClient.Get(ctx, ns, cm); err != nil {
-		return fmt.Errorf("error while getting the pod environment configmap from service cluster: %w", err)
+		// when updating from v0.7.0 straight to v0.10.0, we neither have that ConfigMap (as we use a Secret in version
+		// v0.7.0) nor do we create it (the new labels aren't there yet, so the selector does not match and
+		// operatormanager.OperatorManager.UpdateAllManagedOperators does not call InstallOrUpdateOperator)
+		// we previously aborted here (before the postgresql resource was updated with the new labels), meaning we would
+		// simply restart the loop without solving the problem.
+		if cm, err = r.CreatePodEnvironmentConfigMap(ctx, ns.Namespace); err != nil {
+			return fmt.Errorf("error while creating the missing Pod Environment ConfigMap %v: %w", ns.Namespace, err)
+		}
+		log.Info("mising Pod Environment ConfigMap created!")
 	}
 	cm.Data = data
 	if err := r.SvcClient.Update(ctx, cm); err != nil {
