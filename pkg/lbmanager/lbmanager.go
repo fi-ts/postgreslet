b9e7f68a7aac9a1a8d8a9c2a6b144bd3f9f0c8f4
Philipp Eberle <eberlep@gmail.com>
2023-03-07 10:34:26 +0100

    Make the use  LBSourceRanges configurable

--- pkg/lbmanager/lbmanager.go
+++ pkg/lbmanager/lbmanager.go
@@ -18,6 +18,7 @@ type Options struct {
 	EnableStandbyLeaderSelector bool
 	EnableLegacyStandbySelector bool
 	StandbyClustersSourceRanges []string
+	EnableLBSourceRanges        bool
 }
 
 // LBManager Responsible for the creation and deletion of externally accessible Services to access the Postgresql clusters managed by the Postgreslet.
@@ -61,7 +62,12 @@ func (m *LBManager) CreateSvcLBIfNone(ctx context.Context, in *api.Postgres) err
 			lbIPToUse = ""
 		}
 
-		if err := m.Create(ctx, in.ToSvcLB(lbIPToUse, nextFreePort, m.options.EnableStandbyLeaderSelector, m.options.EnableLegacyStandbySelector, m.options.StandbyClustersSourceRanges)); err != nil {
+		svc := in.ToSvcLB(lbIPToUse, nextFreePort, m.options.EnableStandbyLeaderSelector, m.options.EnableLegacyStandbySelector, m.options.StandbyClustersSourceRanges)
+		if !m.options.EnableLBSourceRanges {
+			// leave empty / disable source ranges
+			svc.Spec.LoadBalancerSourceRanges = []string{}
+		}
+		if err := m.Create(ctx, svc); err != nil {
 			return fmt.Errorf("failed to create Service of type LoadBalancer: %w", err)
 		}
 		return nil
@@ -71,7 +77,13 @@ func (m *LBManager) CreateSvcLBIfNone(ctx context.Context, in *api.Postgres) err
 	// update the selector, and only the selector (we do NOT want the change the ip or port here!!!)
 	svc.Spec.Selector = updated.Spec.Selector
 	// also update the source ranges
-	svc.Spec.LoadBalancerSourceRanges = updated.Spec.LoadBalancerSourceRanges
+	if m.options.EnableLBSourceRanges {
+		// use the given source ranges
+		svc.Spec.LoadBalancerSourceRanges = updated.Spec.LoadBalancerSourceRanges
+	} else {
+		// leave empty / disable source ranges
+		svc.Spec.LoadBalancerSourceRanges = []string{}
+	}
 
 	if err := m.Update(ctx, svc); err != nil {
 		return fmt.Errorf("failed to update Service of type LoadBalancer: %w", err)
