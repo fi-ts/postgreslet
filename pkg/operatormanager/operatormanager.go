097582be735a8e134a14fe78c4435224bb1a93cf
Philipp Eberle <eberlep@gmail.com>
2024-05-16 15:44:26 +0200

    Set spilo_fsgroup to fix tls permission issue

--- pkg/operatormanager/operatormanager.go
+++ pkg/operatormanager/operatormanager.go
@@ -51,6 +51,8 @@ const (
 	SidecarsCMExporterQueriesKey string = "queries.yaml"
 
 	localSidecarsCMName = "postgres-sidecars-configmap"
+
+	spilo_fsgroup = "103"
 )
 
 // operatorPodMatchingLabels is for listing operator pods
@@ -427,6 +429,9 @@ func (m *OperatorManager) editConfigMap(cm *corev1.ConfigMap, namespace string, 
 	cm.Data["replication_username"] = pg.PostgresConfigReplicationUsername
 
 	cm.Data["enable_pod_antiaffinity"] = strconv.FormatBool(options.PodAntiaffinity)
+
+	// set the spilo_fsgroup for correct tls cert permissions
+	cm.Data["spilo_fsgroup"] = spilo_fsgroup
 }
 
 // ensureCleanMetadata ensures obj has clean metadata
