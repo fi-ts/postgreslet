986c2f802ec25c7b1ccaf8d6e9da644a290ff351
Philipp Eberle <eberlep@gmail.com>
2024-06-07 15:02:44 +0200

    Set temporariy default value for testing

--- main.go
+++ main.go
@@ -279,6 +279,7 @@ func main() {
 	viper.SetDefault(enableSuperUserForDBOFlg, false)
 	enableSuperUserForDBO = viper.GetBool(enableSuperUserForDBOFlg)
 
+	viper.SetDefault(tlsClusterIssuerFlg, "pgaas-ca-issuer") // TODO remove, just for testing
 	tlsClusterIssuer = viper.GetString(tlsClusterIssuerFlg)
 	enableCustomTLSCert := false
 	if tlsClusterIssuer != "" {
