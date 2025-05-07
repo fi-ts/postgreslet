93051763aa6d21bac80bb8177f929dfd55e65cf0
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2025-05-07 15:55:07 +0200

    zalando does not support RFC3339

--- api/v1/postgres_types.go
+++ api/v1/postgres_types.go
@@ -102,6 +102,8 @@ const (
 	PostgresConfigReplicationUsername = "standby"
 	PostgresConfigAuditorUsername     = "auditor"
 	PostgresConfigMonitoringUsername  = "monitoring"
+
+	ZALANDO_TIMESTAMP_FORMAT = "2006-01-02T15:04:05-07:00"
 )
 
 var (
@@ -762,7 +764,7 @@ func (p *Postgres) ToUnstructuredZalandoPostgresql(z *zalando.Postgresql, c *cor
 		// make sure there is always a value set. The operator will fall back to CLONE_WITH_BASEBACKUP, which assumes the source db's credentials are existing within the same namespace, which is not the case with the postgreslet.
 		if p.Spec.PostgresRestore.Timestamp == "" {
 			// e.g. 2021-12-07T15:28:00+01:00
-			p.Spec.PostgresRestore.Timestamp = time.Now().Format(time.RFC3339)
+			p.Spec.PostgresRestore.Timestamp = time.Now().Format(ZALANDO_TIMESTAMP_FORMAT)
 		}
 
 		z.Spec.Clone = &zalando.CloneDescription{
