3de2b9d5d0486799b91d1693109eee35bdad0e69
T.v.Dein <git@daemon.de>
2026-08-24 09:08:44 +0200

    Update gen-operator-yaml.sh
    Co-authored-by: Benjamin Ritter <7373184+l0wl3vel@users.noreply.github.com>

--- gen-operator-yaml.sh
+++ gen-operator-yaml.sh
@@ -1,49 +1,21 @@
 #!/bin/bash
 
-# generate merged yaml file with default operator manifests
+# Generate a single corev1.List of the zalando operator's default manifests,
+# as consumed by pkg/operatormanager.
 
-set -e -o pipefail
+set -euo pipefail
 
-tmp=$(mktemp -d)
-operator_version="$1"
-output="$2"
-
-cleanup() {
-    rm -rf "${tmp}"
-}
-
-trap cleanup INT TERM EXIT
-
-if [ -z "$2" ]; then
+if [ -z "${2:-}" ]; then
     >&2 echo "Usage: $0 <operator version> <output yaml file>"
     exit 1
 fi
 
-POSTGRES_OPERATOR_URL="https://raw.githubusercontent.com/zalando/postgres-operator/${operator_version}/manifests"
+url="https://raw.githubusercontent.com/zalando/postgres-operator/$1/manifests"
 
-# output all required yaml files  as concatenated manifests, and split
-# each manifest into a separate file
-(
-    for file in ${POSTGRES_OPERATOR_URL}/configmap.yaml \
-		  	    ${POSTGRES_OPERATOR_URL}/operator-service-account-rbac.yaml \
-			    ${POSTGRES_OPERATOR_URL}/postgres-operator.yaml \
-			    ${POSTGRES_OPERATOR_URL}/api-service.yaml; do
-	    echo "---"
-	    curl -s "$file"
-	done
-) | yq --split-exp "\"${tmp}/\" + \$index + \".yaml\"" --no-doc
-
-# generate yq's load() statements from generated manifest files
-load=""
-for yaml in ${tmp}/*; do
-    if [ -n "$load" ]; then
-        load="$load, "
-    fi
-
-    load="$load load(\"${yaml}\")"
-done
-
-# generate a list with all generated manifests as items
-printf "apiVersion: v1\nkind: List\nitems:\n" | \
-    yq ".items += [ ${load}  ]" > "$output"
+yq eval-all '[.] | {"apiVersion": "v1", "kind": "List", "items": .}' \
+    <(curl -sSf "${url}/configmap.yaml") \
+    <(curl -sSf "${url}/operator-service-account-rbac.yaml") \
+    <(curl -sSf "${url}/postgres-operator.yaml") \
+    <(curl -sSf "${url}/api-service.yaml") \
+    > "$2"
 
